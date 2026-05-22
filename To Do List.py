12b9543c08b29cb50fccc418a2826fa76f2780df
Glen Sihotang <gleensihotang@gmail.com>
2026-05-22 21:02:50 +0700

    Menambahkan fitur show task

--- To Do List.py
+++ To Do List.py
@@ -2,3 +2,14 @@ def create_to_do_list():
     with open ("to_do_list.txt", "w", encoding="utf-8") as file:
         pass
     print("Catatan To Do List berhasil dibuat.\n")
+
+def show_task():
+    with open ("to_do_list.txt", "r", encoding="utf-8") as file:
+        tasks = file.readlines()
+    if tasks:
+        print("Daftar Tugas:")
+        for index, task in enumerate(tasks):
+            print(f"{index + 1}. {task.strip()}")
+        print()
+    else:
+        print("Belum ada tugas yang ditambahkan.\n")
