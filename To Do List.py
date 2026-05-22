4627cb5c2447cd60b09c5b11ac2877a27c81c32c
Glen Sihotang <gleensihotang@gmail.com>
2026-05-22 21:38:15 +0700

    Menambahkan fitur update task, mark task as completed, unmark task as unfinish

--- To Do List.py
+++ To Do List.py
@@ -25,4 +25,61 @@ def add_task():
             task = input(f"Masukkan tugas {i + 1}: ")
             file.write(f"{task} [ ]\n")
 
-add_task()
+def update_task():
+    show_task()
+    print("1. Tandai tugas sebagai selesai")
+    print("2. Menghapus tanda pada tugas sebagai belum selesai")
+    while True:
+        try:
+            update_choice = int(input("Pilih opsi pembaruan (angka saja): "))
+            break
+        except ValueError:
+            print("Input tidak valid. Harap masukkan angka saja.\n")
+
+    if update_choice == 1:
+        mark_task_as_completed()
+    elif update_choice == 2:
+        unmark_task_as_completed()
+    else:
+        print("Pilihan tidak valid.\n")
+
+def mark_task_as_completed(): 
+    while True:
+        try:
+            task_number = int(input("Masukkan nomor tugas yang ingin ditandai sebagai selesai (angka saja): "))
+            break
+        except ValueError:
+            print("Input tidak valid. Harap masukkan angka saja.\n")
+            return
+
+    with open ("to_do_list.txt", "r", encoding="utf-8") as file:
+        tasks = file.readlines()
+    
+    if 0 < task_number <= len(tasks):
+        tasks[task_number - 1] = tasks[task_number - 1].replace("[ ]", "[\u2713]")
+        with open ("to_do_list.txt", "w", encoding="utf-8") as file:
+            file.writelines(tasks)
+        print("Tugas berhasil ditandai sebagai selesai.\n")
+    else:
+        print("Nomor tugas tidak ditemukan.\n")
+
+def unmark_task_as_completed():
+    while True:
+        try:
+            task_number = int(input("Masukkan nomor tugas yang ingin dihapus tandanya sebagai belum selesai (angka saja): "))
+            break
+        except ValueError:
+            print("Input tidak valid. Harap masukkan angka saja.\n")
+
+    with open ("to_do_list.txt", "r", encoding="utf-8") as file:
+        tasks = file.readlines()
+    
+    if 0 < task_number <= len(tasks):
+        tasks[task_number - 1] = tasks[task_number - 1].replace("[\u2713]", "[ ]")
+        with open ("to_do_list.txt", "w", encoding="utf-8") as file:
+            file.writelines(tasks)
+        print("Tanda tugas berhasil dihapus, sekarang menjadi belum selesai.\n")
+    else:
+        print("Nomor tugas tidak ditemukan.\n")
+
+update_task()
